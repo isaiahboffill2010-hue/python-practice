81405f17381a1df91b78d16d3842e1dfa3ed9350
Isaiah Boffill <isaiahboffill2010@gmail.com>
2026-03-27 13:30:08 -0400

    update app

--- pratice2/practice_2.py
+++ pratice2/practice_2.py
@@ -6,3 +6,22 @@ private = 3
 public = 10
 total = private + public
 print("The total of the number of private and public schools are together", total)
+
+percent = 0.5 * 100
+print("The percentage of the students that passed the test is", percent)
+
+post = 55
+print("Posts:", post)
+
+user = "snoopdog"
+print("Username:", user)\
+
+print("Jon"+"athan")
+
+temperature = "3" + "1"
+print(temperature)
+
+private = 600
+public = 400
+total = private = public
+print(total)
