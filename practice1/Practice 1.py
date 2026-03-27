17a109b3bd98ffebe8218e00ce8151acd0c4aebb
Isaiah Boffill <isaiahboffill2010@gmail.com>
2026-03-27 13:08:30 -0400

    update app

---
+++ practice1/Practice 1.py
@@ -0,0 +1,22 @@
+step_1 = "Collect pants"
+step_2 = "Put on pants"
+step_3 = "Collect shirt"
+step_4 = "Put on shirt"
+print(step_1)
+print(step_2)
+print(step_3)
+print(step_4)
+
+My_name = "Isaiah"
+Last_name = "Boffill"
+print(My_name, Last_name)
+
+f = "pizza"
+print("My favtorite food is", f)
+
+best_friend_name = "Logan"
+print("My best friend is", best_friend_name)
+
+bage = 9
+bname = "Logan"
+print("My best friend is", bname,"and he is", bage, "years old")
---
+++ pratice2/practice_2.py
@@ -0,0 +1,8 @@
+sum_of_grades = 460
+students = 5
+print("The average grade is:",sum_of_grades / students)
+
+private = 3
+public = 10
+total = private + public
+print("The total of the number of private and public schools are together", total)
